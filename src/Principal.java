1696c0593da886498069d35e894f2080db45d8fd
Diego Souza <diego.souza@smark.io>
2019-04-16 16:55:41 -0300

    testes de envio da impressora

--- src/Principal.java
+++ src/Principal.java
@@ -1,24 +1,74 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Principal {
 
 	public static void main(String[] args) {
-		PrinterService printerService = new PrinterService();
+//		
+//		ServerSocket server;
+//        Socket client;
+//        InputStream input;
+//
+//        try {
+//            server = new ServerSocket(1010);
+//            client = server.accept();
+//
+//            input = client.getInputStream();
+//            String inputString = Principal.inputStreamAsString(input);
 
-        System.out.println(printerService.getPrinters());
+            PrinterService printerService = new PrinterService();
 
-        String command =  
-                "N\n" + 
-                "q609\n" + 
-                "Q203,26\n" + 
-                "A26,26,0,5,1,2,N,\"HI, MOM!\"\n" + 
-                "P1,1";
-        //print some stuff. Change the printer name to your thermal printer name.
-        printerService.printBytes("ELGIN L42PRO", command.getBytes());
+            System.out.println(printerService.getPrinters());
+            
+            String inputString = "teste";
+
+            String command =
+            		"^XA\n"+
+            				"^FO,20,20^BQ,2,10\n"+
+            				"^FDD03048F,LM,N0123456789,A12AABB,B0006qrcode^FS\n"+
+            				"^XZ\n";
+//                    "N\n" + 
+//            		"S3\n"+
+//            		"D8\n"+
+//            		"ZT\n"+
+//            		"JF\n"+
+//                    "A10,0,0,3,0,1,1,N,\""+inputString+"\"\n" +
+//                    "A20,50,0,3,0,1,1,R,\""+inputString+"\"\n" + 
+//                    "P1\n";
+            //print some stuff. Change the printer name to your thermal printer name.
+            printerService.printBytes("ELGIN L42PRO 2", command.getBytes());
+            
+//            System.out.println(inputString);
+//
+//            client.close();
+//            server.close();
+//        }
+//        catch (Exception e) {
+//            e.printStackTrace();
+//        }
 //
 //        // cut that paper!
 //        byte[] cutP = new byte[] { 0x1d, 'V', 1 };
 //
 //        printerService.printBytes("EPSON-TM-T20II", cutP);
 	}
+	
+	public static String inputStreamAsString(InputStream stream) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(stream));
+        StringBuilder sb = new StringBuilder();
+        String line = null;
+
+        while ((line = br.readLine()) != null) {
+            sb.append(line + "\n");
+        }
+
+        br.close();
+        return sb.toString();
+    }
+
 
 }
