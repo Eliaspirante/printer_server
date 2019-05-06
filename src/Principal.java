d607d588fb2d124776bc7720c5984c81c525360f
Diego Souza <diegosouza@gmail.com>
2019-05-06 00:03:04 -0300

    Adicionando criação de etiquetas

--- src/Principal.java
+++ src/Principal.java
@@ -8,54 +8,88 @@ import java.net.Socket;
 public class Principal {
 
 	public static void main(String[] args) {
-//		
-//		ServerSocket server;
-//        Socket client;
-//        InputStream input;
-//
-//        try {
-//            server = new ServerSocket(1010);
-//            client = server.accept();
-//
-//            input = client.getInputStream();
-//            String inputString = Principal.inputStreamAsString(input);
+		ServerSocket server;
+        Socket client;
+        InputStream input;
 
-            PrinterService printerService = new PrinterService();
+        try {
+        	server = new ServerSocket(8081);
+        
+        	System.out.print("Server started!!");
+        	
+        	while(true) {
+	            client = server.accept();
+	
+	            input = client.getInputStream();
+	            String inputString = Principal.inputStreamAsString(input);
+	
+	            PrinterService printerService = new PrinterService();
+	
+	            System.out.println(printerService.getPrinters());
+	            
+	            StringBuilder texto = new StringBuilder();
+	            
+	            String[] datas = inputString.split(";");
+	            
+	            for(int i = 0; i < datas.length; i++) {
+		            String[] data = datas[i].split(":");
+		            String type = data[0];
+		            String data1 = data[1];
+		            
+		            texto.append("N\n");
+		            texto.append("JF\n");
+		            texto.append("ZB\n");
+		            texto.append("Q100,24\n");
+		            texto.append("q760\n");
+	//	            texto.append("B300,52,0,E80,2,2,33,B,\"1234567\"\n");
+	//	            texto.append("B300,52,0,1,3,4,20,N,\"RMS130\"\n");
+	//	            texto.append("B300,52,0,K,4,6,25,B,\"RMS130\"\n");
+	//	            texto.append("A610,52,0,4,1,1,N,\"RMS130\"\n");
+		            texto.append("A80,0,0,4,1,1,N,\""+data1+"\"\n");
+	//	            texto.append("B230,0,0,1,3,4,20,N,\"RMS130\"\n");
+		            System.out.println(type);
+		            if(type.equals("EAN")) {
+		            	String codigo = data[2].replace("\n", "");
+		            	System.out.println("ENTROU EAN");
+		            	System.out.println(data[2].replace("\n", ""));
+		            	texto.append("B250,0,0,UE0,3,3,30,B,\""+codigo+"\"\n");
+		            }
+		            if(type.equals("EAN128")) {
+		            	String codigo = data[2].replace("\n", "");
+		            	System.out.println("ENTROU EAN128");
+		            	System.out.println(data[2].replace("\n", ""));
+		            	texto.append("B270,0,0,1E,2,3,30,B,\""+codigo+"\"\n");
+		            }
+		            if(type.equals("2OF5")) {
+		            	String codigo = data[2].replace("\n", "");
+		            	System.out.println("ENTROU 2OF5");
+		            	System.out.println(data[2].replace("\n", ""));
+		            	texto.append("B300,0,0,2D,5,6,30,B,\""+codigo+"\"\n");
+		            }
+		            if(type.equals("CODE39")) {
+		            	String codigo = data[2].replace("\n", "");
+		            	System.out.println("ENTROU CODE");
+		            	System.out.println(data[2]);
+		            	texto.append("B250,0,0,1,3,4,20,N,\""+codigo+"\"\n");
+		            }
+		            texto.append("P1\n");
+	            }
+	            
+	            printerService.printBytes("L42PRO", texto.toString().getBytes());
+	            
+	            System.out.println("Print data!!");
+	            System.out.println(inputString);
+	
+	            client.close();
+        	}
+        	
+        	
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            
+        }
 
-            System.out.println(printerService.getPrinters());
-            
-            String teste = "TESTE DE ABOBOBOBORA";
-            String teste2 = "aqui novo";
-            StringBuilder texto = new StringBuilder("N\n"); //  (limpa o buffer de impress�o, para ser iniciado um novo arquivo BPLB)
-//            texto.append("D13\n");  //(configura a Densidade ou aquecimento da cabe�a de impress�o para o valor 9)
-//            texto.append("S3\n");  //(configura a Velocidade de impress�o para 3 pol./seg)
-//            texto.append("JF\n");  //(habilita o �backfeed� para que ao final da impress�o, o espa�o entre etiquetas pare na serrilha)
-//            texto.append("ZT\n");  //(indica que a impress�o deve inciar a partir do topo, ou seja, de cabe�a para baixo)
-//            texto.append("Q88,24\n");
-//            texto.append("q760\n");
-//            texto.append("A0,20,0,3,1,1,N," + '"' +  teste  + '"' + "\n");
-//            texto.append("A40,20,0,3,1,1,N," + '"' +  teste  + '"' + "\n");
-//            texto.append("A250,20,0,3,1,1,N," + '"' +  teste2  + '"' + "\n");
-            texto.append("teste222    teste3");
-            texto.append("P1\n");
-            //print some stuff. Change the printer name to your thermal printer name.
-            printerService.printBytes("L42PRO", texto.toString().getBytes());
-            		
-//            printerService.printString("ELGIN L42Pro","teste");
-            
-//            System.out.println(inputString);
-//
-//            client.close();
-//            server.close();
-//        }
-//        catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//
-//        // cut that paper!
-//        byte[] cutP = new byte[] { 0x1d, 'V', 1 };
-//
-//        printerService.printBytes("EPSON-TM-T20II", cutP);
 	}
 	
 	public static String inputStreamAsString(InputStream stream) throws IOException {
---
+++ src/PrincipalLocal.java
@@ -0,0 +1,108 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class PrincipalLocal {
+
+	public static void main(String[] args) {
+		ServerSocket server;
+        Socket client;
+        InputStream input;
+
+        try {
+//        	server = new ServerSocket(8081);
+//        
+//        	System.out.print("Server started!!");
+        	
+//        	while(true) {
+//	            client = server.accept();
+	
+//	            input = client.getInputStream();
+//	            String inputString = Principal.inputStreamAsString(input);
+	
+        		String inputString = "EAN128:RSM130:RSM130";
+	            PrinterService printerService = new PrinterService();
+	
+	            System.out.println(printerService.getPrinters());
+	            
+	            StringBuilder texto = new StringBuilder();
+	            
+	            String[] data = inputString.split(":");
+	            String type = data[0];
+	            String data1 = data[1];
+	            
+	            texto.append("N\n");
+	            texto.append("D30\n");
+	            texto.append("S3\n");
+	            texto.append("JF\n");
+	            texto.append("ZB\n");
+	            texto.append("Q100,24\n");
+	            texto.append("q760\n");
+//	            texto.append("B300,52,0,E80,2,2,33,B,\"1234567\"\n");
+//	            texto.append("B300,52,0,1,3,4,20,N,\"RMS130\"\n");
+//	            texto.append("B300,52,0,K,4,6,25,B,\"RMS130\"\n");
+//	            texto.append("A610,52,0,4,1,1,N,\"RMS130\"\n");
+	            texto.append("A80,0,0,4,1,1,N,\""+data1+"\"\n");
+//	            texto.append("B230,0,0,1,3,4,20,N,\"RMS130\"\n");
+	            System.out.println(type);
+	            if(type.equals("EAN")) {
+	            	String codigo = data[2].replace("\n", "");
+	            	System.out.println("ENTROU EAN");
+	            	System.out.println(data[2].replace("\n", ""));
+	            	texto.append("B250,0,0,UE0,3,3,30,B,\""+codigo+"\"\n");
+	            }
+	            if(type.equals("EAN128")) {
+	            	String codigo = data[2].replace("\n", "");
+	            	System.out.println("ENTROU EAN128");
+	            	System.out.println(data[2].replace("\n", ""));
+	            	texto.append("B270,0,0,1E,2,3,30,B,\""+codigo+"\"\n");
+	            }
+	            if(type.equals("2OF5")) {
+	            	String codigo = data[2].replace("\n", "");
+	            	System.out.println("ENTROU 2OF5");
+	            	System.out.println(data[2].replace("\n", ""));
+	            	texto.append("B300,0,0,2D,5,6,30,B,\""+codigo+"\"\n");
+	            }
+	            if(type.equals("CODE39")) {
+	            	String codigo = data[2].replace("\n", "");
+	            	System.out.println("ENTROU CODE");
+	            	System.out.println(data[2]);
+	            	texto.append("B250,0,0,1,3,4,20,N,\""+codigo+"\"\n");
+	            }
+	            texto.append("P1\n");
+	            
+	            printerService.printBytes("L42PRO", texto.toString().getBytes());
+	            
+	            System.out.println("Print data!!");
+	            System.out.println(inputString);
+	
+//	            client.close();
+//        	}
+        	
+        	
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            
+        }
+
+	}
+	
+	public static String inputStreamAsString(InputStream stream) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(stream));
+        StringBuilder sb = new StringBuilder();
+        String line = null;
+
+        while ((line = br.readLine()) != null) {
+            sb.append(line + "\n");
+        }
+
+        br.close();
+        return sb.toString();
+    }
+
+
+}
